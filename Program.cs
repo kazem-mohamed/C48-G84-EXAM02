d863a74cac7dfae6c7b3c087f9227e5b21474882
Kazem Mohamed <xkazemx15@gmail.com>
2026-09-19 20:04:47 +0300

    feat: solve Question 6

--- Program.cs
+++ Program.cs
@@ -52,6 +52,137 @@ public abstract partial class Question : ICloneable, IComparable<Question>
 }
 #endregion
 
+#region Question 6 - Exam Base Class
+public abstract class Exam : ICloneable, IComparable<Exam>
+{
+    public TimeSpan TimeOfExam { get; set; }
+    public Question[] Questions { get; set; } = Array.Empty<Question>();
+
+    public int NumberOfQuestions => Questions.Length;
+
+    protected TimeSpan TakenTime { get; private set; }
+
+    protected int TotalMarks
+    {
+        get
+        {
+            int total = 0;
+
+            foreach (Question question in Questions)
+            {
+                total += question.Mark;
+            }
+
+            return total;
+        }
+    }
+
+    protected Exam() : this(TimeSpan.FromHours(1), Array.Empty<Question>()) { }
+
+    protected Exam(TimeSpan timeOfExam, Question[] questions)
+    {
+        TimeOfExam = timeOfExam;
+        Questions = questions ?? Array.Empty<Question>();
+    }
+
+    public abstract void ShowExam();
+
+    protected int[] AskQuestions()
+    {
+        int[] chosenAnswers = new int[Questions.Length];
+        DateTime startedAt = DateTime.Now;
+
+        for (int i = 0; i < Questions.Length; i++)
+        {
+            Question question = Questions[i];
+
+            Console.WriteLine($"Question {i + 1}: {question.Body}");
+            Console.WriteLine(question);
+
+            foreach (Answer answer in question.AnswerList)
+            {
+                Console.WriteLine(answer);
+            }
+
+            chosenAnswers[i] = ReadAnswerId(question);
+            Console.WriteLine();
+        }
+
+        TakenTime = DateTime.Now - startedAt;
+        return chosenAnswers;
+    }
+
+    // Keeps asking until the student types one of the listed answer numbers.
+    private static int ReadAnswerId(Question question)
+    {
+        while (true)
+        {
+            Console.WriteLine("Enter your answer ID:");
+            string input = Console.ReadLine();
+
+            // Nothing left to read: leave the question unanswered.
+            if (input == null)
+            {
+                return 0;
+            }
+
+            if (int.TryParse(input, out int answerId) &&
+                Array.Exists(question.AnswerList, answer => answer.AnswerId == answerId))
+            {
+                return answerId;
+            }
+
+            Console.WriteLine($"Please enter a number between 1 and {question.AnswerList.Length}.");
+        }
+    }
+
+    protected void ShowGrade(int[] chosenAnswers)
+    {
+        int grade = 0;
+
+        for (int i = 0; i < Questions.Length; i++)
+        {
+            if (Questions[i].IsAnsweredCorrectly(chosenAnswers[i]))
+            {
+                grade += Questions[i].Mark;
+            }
+        }
+
+        Console.WriteLine($"Your Grade is {grade} from {TotalMarks}");
+        Console.WriteLine($"Time = {TakenTime}");
+        Console.WriteLine("Thank you");
+    }
+
+    public virtual object Clone()
+    {
+        Exam copy = (Exam)MemberwiseClone();
+        copy.Questions = new Question[Questions.Length];
+
+        for (int i = 0; i < Questions.Length; i++)
+        {
+            copy.Questions[i] = (Question)Questions[i].Clone();
+        }
+
+        return copy;
+    }
+
+    public int CompareTo(Exam other)
+    {
+        if (other == null)
+        {
+            return 1;
+        }
+
+        return NumberOfQuestions.CompareTo(other.NumberOfQuestions);
+    }
+
+    public override string ToString()
+    {
+        return $"{GetType().Name} - Time: {TimeOfExam.TotalMinutes} minutes - Questions: {NumberOfQuestions}";
+    }
+}
+#endregion
+
 #region Question 5 - Question Answers
 public abstract partial class Question
 {
@@ -149,12 +280,42 @@ public enum ExamType
     Final = 2,
 }
 
-public class FinalExam
+public class FinalExam : Exam
 {
+    public FinalExam() { }
+
+    public FinalExam(TimeSpan timeOfExam, Question[] questions)
+        : base(timeOfExam, questions) { }
+
+    public override void ShowExam()
+    {
+        Console.WriteLine("Final Exam");
+        AskQuestions();
+    }
 }
 
-public class PracticalExam
+public class PracticalExam : Exam
 {
+    public PracticalExam() { }
+
+    public PracticalExam(TimeSpan timeOfExam, Question[] questions)
+        : base(timeOfExam, questions)
+    {
+        foreach (Question question in Questions)
+        {
+            if (question is not MCQQuestion)
+            {
+                throw new ArgumentException(
+                    "A practical exam accepts MCQ questions only.", nameof(questions));
+            }
+        }
+    }
+
+    public override void ShowExam()
+    {
+        Console.WriteLine("Practical Exam");
+        AskQuestions();
+    }
 }
 #endregion
 
