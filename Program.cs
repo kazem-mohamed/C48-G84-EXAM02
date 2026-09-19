18f1e2a9ea92a04cb96421993f702d6bcd2dd1ba
Kazem Mohamed <xkazemx15@gmail.com>
2026-09-19 20:03:43 +0300

    feat: solve Question 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,41 @@
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+#region Question 1 - Question Class
+public abstract partial class Question : IComparable<Question>
+{
+    public string Header { get; set; }
+    public string Body { get; set; }
+    public int Mark { get; set; }
+
+    protected Question() : this("Question", string.Empty, 1) { }
+
+    protected Question(string header, string body) : this(header, body, 1) { }
+
+    protected Question(string header, string body, int mark)
+    {
+        Header = header;
+        Body = body;
+        Mark = mark;
+    }
+
+    public int CompareTo(Question other)
+    {
+        if (other == null)
+        {
+            return 1;
+        }
+
+        return Mark.CompareTo(other.Mark);
+    }
+
+    public override string ToString()
+    {
+        return $"{Header}:   Mark {Mark}";
+    }
+}
+#endregion
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+    }
+}
